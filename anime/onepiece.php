d1dc753a7900fb6dc7e8b75f514dab7622476960
41M3Dev <102484378+41M3Dev@users.noreply.github.com>
2022-04-30 03:00:37 +0200

    ImagaMaj

---
+++ anime/onepiece.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<?php 
+    // Connexion et choix de la base de données
+   require_once '../config/config.php';
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Citation</title>
+    </head>
+    <body>
+        <div class="header">
+            <a href="../index.html" class="logo">Citation</a>
+            <div class="header-right">
+                <a href="../php/formannonce.php">Déposer une annonce</a>
+                <a href="onepiece.php">One Piece</a>
+                <a href="naruto.php">Naruto</a>
+                <a href="#avoire">AVOIR</a>
+                <a href="#avoire">AVOIR</a>
+            </div>
+        </div>
+        
+        <h1>Citation de Naruto(Shippuden)</h1>
+        <?php // Affihce les valeur de la base de donnée
+$sql="SELECT `id`, `anime`, `auteur`, `citation` FROM `citation` WHERE `anime` = 'OnePiece'";
+            $result = $bdd->query($sql);
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)){?>
+        <article class="article">
+            <blockquote>
+                <p><?php echo "{$row['citation']}" ?></p>
+                <cite><?php echo "{$row['auteur']}" ?></cite>
+                <?php $nom = strtoupper($row['auteur'])  ?> <div class="blockquote-author-image" style="--image: url('../img/<?php echo "$nom.jpg" ?>')"> </div> <?php ?>
+            </blockquote>
+        </article>
+        <?php
+            }
+        ?>
+
+    </body>
+</html>
--- php/formannonce.php
+++ php/formannonce.php
@@ -1,6 +1,6 @@
 <?php 
     // Connexion et choix de la base de données
-   // require_once '../config/config.php';
+   require_once '../config/config.php';
 ?>
 
 <!DOCTYPE html>
@@ -14,25 +14,45 @@
 </head>
 <body>
         <div class="header">
-            <a href="index.html" class="logo">Citation</a>
+            <a href="../index.html" class="logo">Citation</a>
             <div class="header-right">
-                <a href="anime/naruto.html">Naruto</a>
-                <a href="php/formannonce.php">Déposer une annonce</a>
+                <a href="formannonce.php">Déposer une annonce</a>
+                <a href="../anime/onepiece.php">One Piece</a>
+                <a href="../anime/naruto.php">Naruto</a>
                 <a href="#avoire">AVOIR</a>
                 <a href="#avoire">AVOIR</a>
             </div>
         </div>
         <div class="container">
-        <form action="/action_page.php">
-          <label for="fname">Auteur</label>
-          <input type="text" id="fname" name="firstname" placeholder="Nom de l'auteur">
-      
-          <label for="subject">Citation</label>
-          <textarea id="subject" name="subject" placeholder="Votre citation" style="height:200px"></textarea>
-      
-          <input type="submit" value="Submit">
+        <form action="" method="GET">
+            <label for="choix_bieres">Auteur:</label>
+            <input name="auteur" list="auteur" type="text" id="fname">
+            <datalist id="auteur">
+                <option value="Naruto">
+                <option value="Itachi">
+                <option value="Gaara">
+                <option value="Hinata">
+            </datalist>
+            <label for="subject">Citation</label>
+            <textarea id="subject" name="citation" placeholder="Votre citation" style="height:200px"></textarea>
+            <input type="submit" value="Envoyer" name="envoyer">
         </form>
       </div>
-    
 </body>
 </html>
+
+<?php
+    if(isset($_GET['envoyer'])){
+            if(!empty($_GET['auteur']) && !empty($_GET['citation']))
+            {
+        //Récpétration des valeur
+            $auteur = htmlspecialchars($_GET['auteur']);
+            $citation = htmlspecialchars($_GET['citation']);
+        // Insertion des valeur dans la base de donnée
+            $requete = "INSERT INTO `citation`(`auteur`,`citation`) VALUES (?,?)";
+            $res = $bdd->prepare($requete);
+            $exec = $res->execute(array($auteur,$citation));
+            }
+        }
+    
+?>
